6363de1b08fa9e2f2f229d94e47f274bbf2dd2ba
Prateek Gupta <prateekgupta2002@gmail.com>
2024-05-25 17:19:54 +0530

    Managed add contact form data

---
+++ Smart Contact Manager/src/main/java/com/scm/services/ContactService.java
@@ -0,0 +1,28 @@
+package com.scm.services;
+
+import java.util.List;
+
+import com.scm.entities.Contact;
+
+public interface ContactService {
+//save contacts
+	Contact save(Contact contact);
+	
+	//update contacts
+	Contact update(Contact contact);
+	
+	//get contacts
+	List<Contact> getAll();
+	
+	//get contact by id
+	Contact getById(String id);
+	
+	//delete contact
+	void delete(String id);
+	
+	//search contact
+	List<Contact> search(String name, String email, String phoneNumber);
+	
+	//get contacts by user id
+	List<Contact> getByUserId(String userId);
+}
